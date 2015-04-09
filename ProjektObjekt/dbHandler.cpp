f885c1b675f83cfa1811f7712f22d3a78d9b29c4
denilsson2 <denilsson2@gmail.com>
2015-04-09 13:32:41 +0200

    Revert "Add Course class."
    This reverts commit 6833b77562f5196f82397e6dbf09612b0c8e5916.

--- ProjektObjekt/dbHandler.cpp
+++ ProjektObjekt/dbHandler.cpp
@@ -14,8 +14,8 @@ DbCommand^ dbHandler::getCommand()
 {
 	// Get connection
 	_connection = _factory->CreateConnection();
-	//_connection->ConnectionString = "Data Source=MACBOOK-PRO;Initial Catalog=DBanan14hj;Integrated Security=False;User ID=anan14hj;Password=Jwcg998;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
-	_connection->ConnectionString = "Data Source=NILSSONS_PC;Initial Catalog=DBnida1300;Integrated Security=False;User ID=nida1300;Password=Ecod558;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+	_connection->ConnectionString = "Data Source=MACBOOK-PRO;Initial Catalog=DBanan14hj;Integrated Security=False;User ID=anan14hj;Password=Jwcg998;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+	//_connection->ConnectionString = "Data Source=NILSSONS_PC;Initial Catalog=DBnida1300;Integrated Security=False;User ID=nida1300;Password=Ecod558;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
 	_connection->Open();
 	
 	// Create DB command
