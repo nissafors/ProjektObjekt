4d208b4f229099b6a34e54760dfe4b40b4be1128
nissafors <aa.gpost@gmail.com>
2015-04-12 19:28:53 +0200

    Teacher has static getAllTeachers() function.

--- ProjektObjekt/Teacher.cpp
+++ ProjektObjekt/Teacher.cpp
@@ -1,17 +1,31 @@
 #include "stdafx.h"
 #include "Teacher.h"
 
+Teacher::Teacher()
+{
+	_username = "";
+	_userType = _user_t::teacher;
+	_salary = 0;
+	_employeeNumber = 0;
+}
+
 Teacher::Teacher(String^ username)
 {
 	_username = username;
 	_userType = _user_t::teacher;
 	_salary = 0;
+	_employeeNumber = 0;
 	readFromDB();
 }
 
 int Teacher::getSalary()
 {
 	return _salary;
+}
+
+int Teacher::getEmployeeNumber()
+{
+	return _employeeNumber;
 }
 
 void Teacher::readFromDB()
@@ -28,4 +42,25 @@ void Teacher::readFromDB()
 	if (!reader->IsDBNull(1))
 		_salary = reader->GetInt16(1);
 	reader->Close();
+}
+
+vector<Teacher^>^ Teacher::getAllTeachers()
+{
+	dbHandler dbh;
+	DbCommand^ command = dbh.getCommand();
+	command->CommandText = "SELECT namn, lön, anstNr FROM Personal";
+	DbDataReader^ reader = command->ExecuteReader();
+	vector<Teacher^>^ allTeachers = gcnew vector<Teacher^>();
+	Teacher^ teacher;
+	while (reader->Read())
+	{
+		teacher = gcnew Teacher();
+		teacher->_name = reader->GetString(0);
+		teacher->_salary = reader->GetInt32(1);
+		teacher->_employeeNumber = reader->GetInt32(2);
+		allTeachers->push_back(teacher);
+		delete teacher;
+	}
+
+	return allTeachers;
 }
--- ProjektObjekt/Teacher.h
+++ ProjektObjekt/Teacher.h
@@ -1,14 +1,20 @@
 #pragma once
 #include "User.h"
+#include <cliext/vector>
+using namespace cliext;
 
 public ref class Teacher : public User
 {
 public:
+	Teacher();
 	Teacher(String^ username);
 	int getSalary();
+	int getEmployeeNumber();
+	static vector<Teacher^>^ getAllTeachers();
 protected:
 	void readFromDB() override;
 private:
+	int _employeeNumber;
 	int _salary;
 };
 
